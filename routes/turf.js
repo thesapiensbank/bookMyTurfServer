73a80d6a93bbe59845da8365a176065a185b9960
optimus789 <vinaysudrik@gmail.com>
2021-05-04 01:01:23 +0530

    Dashboard turf frontend part create turf completed

--- routes/turf.js
+++ routes/turf.js
@@ -9,24 +9,58 @@ router.route('/').get((req, res) => {
 });
 
 router.route('/add').post((req, res) => {
-  console.log(req)
-  if (!isAdmin(req)) { //remove this !isAdmin on production
-    const status = req.body.status;
+  // console.log(req)
+  if (isAdmin(req)) { //remove this !isAdmin on production
+
+    console.log(req.body)
+    // console.log("This sports: ",Array.isArray(req.body.sports_Football))
+    const status = req.body.status==='on'?true:false;
     const name = req.body.name;
     const website = req.body.website;
     const mobile = req.body.mobile;
-    const location = req.body.location;
+    const location = (req.body.location).match(/[-]{0,1}[\d]*[.]{0,1}[\d]+/g);
     const address1 = req.body.address1;
     const address2 = req.body.address2;
     const city = req.body.city;
     const pincode = req.body.pincode;
     const state = req.body.state;
-    const sports = req.body.sports;
-    const features = req.body.features;
+
+    let sports = [];
+    let features = [];
+    let body = req.body;
+    for(var reqname in body){
+      // for sports
+      if(reqname.startsWith("sports")){
+            sportName = reqname.split('_')[1];
+            console.log(sportName)
+            sports.push({ name: sportName, value: Array.isArray(body[reqname])?true:false });
+          }
+      // for features
+      if(reqname.startsWith("features")){
+            sportName = reqname.split('_')[1];
+            console.log(sportName)
+            features.push({ name: sportName, value: Array.isArray(body[reqname])?true:false });
+          }
+    }
+
     const slots = req.body.slots;
-    const turftype = req.body.turftype;
-    const imagefile = req.body.imagefile;
-    const date = Date.parse(req.body.date);
+    // const turftype = req.body.turftype;
+    const turftype = [
+      {
+          "name":"Basketball",
+          "area":{
+              "height":"5",
+              "width":"5",
+              "units":"ft"
+          },
+          "operatinghours":["4.00 am to 5.00 am","5.00 am to 6.00 am","6.00 am to 7.00 am","7.00 am to 8.00 am"],
+          "bookedhours":["9.00 am to 10.00 am","11.00 am to 12.00 am","7.00 am to 8.00 am"]
+      }
+  ];
+    // const imagefile = req.body.imagefile;
+    const imagefile = ["img/slide.jpg","img/slide2.jpg"];
+    // const date = Date.parse(req.body.date); 
+    
 
     const newTurf = new Turf({
       status,
@@ -44,7 +78,6 @@ router.route('/add').post((req, res) => {
       slots,
       turftype,
       imagefile,
-      date,
     });
 
     newTurf
